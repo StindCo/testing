f25aa3642e697d31cd7d0fede13198f68d9b6be5
Stéphane Ngoyi <47933258+StindCo@users.noreply.github.com>
2023-11-27 18:37:24 +0100

    Update Axios.ts

--- src/shared/fetchers/Axios.ts
+++ src/shared/fetchers/Axios.ts
@@ -1,33 +1,33 @@
 import axios from "axios";
 
 export const UsersManagementFetcher = axios.create({
-  baseURL: "http://50.116.39.140:8080/v1/users-management",
+  baseURL: "https://50.116.39.140:8080/v1/users-management",
   timeout: 40000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
 
 export const UsersManagementFetcherSecurity = (token: string) => {
   return axios.create({
-    baseURL: "http://50.116.39.140:8080/v1/users-management",
+    baseURL: "https://50.116.39.140:8080/v1/users-management",
     timeout: 40000,
     headers: { "x-jwt-token": token },
   });
 };
 
 export const SchemaDiscover = axios.create({
-  baseURL: "http://50.116.39.140:8080/v1/schema-discover",
+  baseURL: "https://50.116.39.140:8080/v1/schema-discover",
   timeout: 40000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
 
 export const ProjectManager = axios.create({
-  baseURL: "http://50.116.39.140:8080/v1/project-manager",
+  baseURL: "https://50.116.39.140:8080/v1/project-manager",
   timeout: 40000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
 
 export const DataLoader = axios.create({
-  baseURL: "http://50.116.39.140:8080/v1",
+  baseURL: "https://50.116.39.140:8080/v1",
   timeout: 40000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
